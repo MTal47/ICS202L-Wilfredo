24e0961cef605a59ae32dd0b53487d89cfc5709b
MTal47 <1120266@est.intec.edu.do>
2025-02-14 10:54:32 -0400

    Create 0x267-programa4.c

---
+++ 0x267-programa4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+void function() { 
+int var = 5;
+static int static_var = 5; 
+printf("\t[in function] var = %d\n", var);
+printf("\t[in function] static_var = %d\n", static_var);
+var++; 
+static_var++; 
+}
+int main() {
+int i;
+static int static_var = 1337; 
+for(i=0; i < 5; i++) { 
+printf("[in main] static_var = %d\n", static_var);
+function(); 
+}
+}
+
+/*output:
+[in main] static_var = 1337
+[in function] var = 5
+[in function] static_var = 5
+[in main] static_var = 1337
+[in function] var = 5
+[in function] static_var = 6
+[in main] static_var = 1337
+[in function] var = 5
+[in function] static_var = 7
+[in main] static_var = 1337
+[in function] var = 5
+[in function] static_var = 8
+[in main] static_var = 1337
+[in function] var = 5
+[in function] static_var = 9
+*/
