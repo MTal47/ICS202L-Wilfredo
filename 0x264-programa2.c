43c62527a8e74af3827f5c147cfa89c7e5ed94ef
MTal47 <1120266@est.intec.edu.do>
2025-02-14 10:41:56 -0400

    Create 0x264-programa2.c

---
+++ 0x264-programa2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+int main() {
+char message[10];
+int count, i;
+strcpy(message, "Hello, world!");
+printf("Repeat how many times? ");
+scanf("%d", &count);
+for(i=0; i < count; i++)
+printf("%3d - %s\n", i, message);
+}
+
+/*
+output:
+Repeat how many times? 11
+0 - Hello, world!
+1 - Hello, world!
+2 - Hello, world!
+3 - Hello, world!
+4 - Hello, world!
+5 - Hello, world!
+6 - Hello, world!
+7 - Hello, world!
+8 - Hello, world!
+9 - Hello, world!
+10 - Hello, world!
+  */
